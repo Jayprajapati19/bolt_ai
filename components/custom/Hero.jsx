824b51e10865e30f5d2b54ea75ef2532857348e2
Jayprajapati19 <jayp37679@gmail.com>
2025-01-23 14:14:39 +0530

    Add UserDetailContext, Convex client provider, and SignInDialog; integrate Google OAuth and update layout structure

--- components/custom/Hero.jsx
+++ components/custom/Hero.jsx
@@ -1,15 +1,24 @@
 "use client"
 import { MessagesContext } from '@/context/MessagesContext';
+import { UserDetailContext } from '@/context/UserDetailContext';
 import Colors from '@/data/Colors';
 import Lookup from '@/data/Lookup'
 import { ArrowRight, Link } from 'lucide-react'
 import React, { useContext, useState } from 'react'
+import SignInDialog from './SignInDialog';
 
 function Hero() {
     const [userInput, setUserInput] = useState();
     const { messages, setMessages } = useContext(MessagesContext)
+    const { userDetail, setUserDetail } = useContext(UserDetailContext)
+    const [openDialog, setOpenDialog] = useState(false);
 
     const onGenerate = (input) => {
+        if (!userDetail?.name) {
+            setOpenDialog(true);
+            return;
+        }
+
         setMessages({
             role: 'user',
             content: input
@@ -51,6 +60,8 @@ function Hero() {
                     >{suggestion}</h2>
                 ))}
             </div>
+            <SignInDialog openDialog={openDialog}
+                closeDialog={(v) => setOpenDialog(v)} />
         </div>
     )
 }
---
+++ components/custom/SignInDialog.jsx
@@ -0,0 +1,70 @@
+import React, { useContext, useState } from 'react'
+
+import {
+    Dialog,
+    DialogContent,
+    DialogDescription,
+    DialogHeader,
+    DialogTitle,
+    DialogTrigger,
+} from "@/components/ui/dialog"
+import Lookup from '@/data/Lookup'
+import { Button } from '../ui/button'
+import { useGoogleLogin } from '@react-oauth/google';
+import axios from 'axios';
+import { UserDetailContext } from '@/context/UserDetailContext';
+
+
+function SignInDialog({ openDialog, closeDialog }) {
+
+    const { userDeatail, setUserDetail } = useContext(UserDetailContext);
+
+
+
+
+    const googleLogin = useGoogleLogin({
+        onSuccess: async (tokenResponse) => {
+            console.log(tokenResponse);
+            const userInfo = await axios.get(
+                'https://www.googleapis.com/oauth2/v3/userinfo',
+                { headers: { Authorization: 'Bearer ' + tokenResponse?.access_token } },
+            );
+
+            console.log(userInfo);
+            setUserDetail(userInfo?.data)
+
+            // sace this inside our Database
+
+            closeDialog(false);
+
+        },
+        onError: errorResponse => console.log(errorResponse),
+    });
+
+    return (
+        <Dialog open={openDialog} onOpenChange={closeDialog}>
+            <DialogContent>
+                <DialogHeader>
+                    <DialogTitle></DialogTitle>
+                    <DialogDescription >
+                        <div className='flex flex-col items-center justify-center gap-3'>
+                            <h2 className='font-bold text-2xl text-white text-center'>{Lookup.SIGNIN_HEADING}</h2>
+                            <p className='mt-2 text-center '>{Lookup.SIGNIN_SUBHEADING}</p>
+                            <Button
+                                onClick={googleLogin}
+                                className='bg-blue-500 text-white hover:bg-blue-400
+                            mt-3 text-xl
+                            '>Sign In with Google
+                            </Button>
+                            <p>{Lookup?.SIGNIn_AGREEMENT_TEXT}</p>
+                        </div>
+                    </DialogDescription>
+                </DialogHeader>
+            </DialogContent>
+        </Dialog>
+
+    )
+}
+
+
+export default SignInDialog
